bffd70da738c911db1bc4368b38c3d96dae58215
Michael-Quintero <michael.quintero@rackspace.com>
2024-07-08 14:17:51 -0500

    v1.1 - Updated the cve_checker.sh
    Updates:
    - Can now accept a list of CVE to be set at a persistent variable
    - Will now also check if the CVE is available from a list of updates

--- cve_checker.sh
+++ cve_checker.sh
@@ -1,39 +1,74 @@
-##################################################################################################################################################
+#############################################################################################################################
 # Script Name: cve_checker.sh
+# Version: 1.1
 # Author: michael.quintero@rackspace.com
-# Description: Used to search for installed CVEs related to patching for our patching team (in relation to remediation)
-##################################################################################################################################################
+# Description: Searches for installed CVEs & checks if those CVEs are available for install. Only supports RHEL at the moment
+#############################################################################################################################
 
 #!/bin/bash
 
-CVE_LIST=("CVE-2022-42896" "CVE-2023-4921" "CVE-2023-38409" "CVE-2023-45871" "CVE-2024-1086" "CVE-2024-26602" "CVE-2023-42753")
-found_count=0
+check_cve_update() {
+    local CVE=$1
+    echo "Checking for available update for CVE: $CVE"
+    found=$(yum updateinfo list cves | grep -c $CVE)
+    if [ "$found" -ne "0" ]; then
+        echo "Update available for $CVE."
+        ((found_update_count++))
+    else
+        echo "No update found for $CVE."
+    fi
+    echo ""
+}
+
+check_cve_installed() {
+    local CVE=$1
+    echo "Checking if CVE is installed: $CVE"
+    found=$(rpm -qa --changelog | grep -c $CVE)
+    if [ "$found" -ne "0" ]; then
+        echo "Patch for $CVE found in installed packages."
+        ((found_installed_count++))
+    else
+        echo "No patch for $CVE found in installed packages."
+    fi
+    echo ""
+}
+
+read -p "Enter CVEs separated by space: " -a CVE_LIST
+
+found_update_count=0
+found_installed_count=0
 total_cves=${#CVE_LIST[@]}
 
 hostname
 date
 
 for CVE in "${CVE_LIST[@]}"; do
-    echo "Checking installed packages for $CVE:"
-    found=$(rpm -qa --changelog | grep -c $CVE)
-    if [ "$found" -ne "0" ]; then
-        echo "Patch for $CVE found in installed packages."
-        ((found_count++))
-    else
-        echo "No patch for $CVE found in installed packages."
-    fi
-    echo ""
+    check_cve_update $CVE
+    check_cve_installed $CVE
 done
 
-echo "Search completed for $total_cves CVEs, with $found_count CVEs patched on the system."
+echo "Search completed for $total_cves CVEs."
+echo "$found_update_count CVEs have available updates."
+echo "$found_installed_count CVEs are patched on the system."
 
-if [ $found_count -eq $total_cves ]; then
-    state="all"
-elif [ $found_count -gt 0 ]; then
-    state="some"
+if [ $found_update_count -eq $total_cves ]; then
+    update_state="all"
+elif [ $found_update_count -gt 0 ]; then
+    update_state="some"
 else
-    state="none"
+    update_state="none"
 fi
 
-echo "Result: $state of the CVEs were patched on the system."
-rm -- "$0" 
+if [ $found_installed_count -eq $total_cves ]; then
+    installed_state="all"
+elif [ $found_installed_count -gt 0 ]; then
+    installed_state="some"
+else
+    installed_state="none"
+fi
+
+echo "Result: $update_state of the CVEs have available updates."
+echo "Result: $installed_state of the CVEs are patched on the system."
+
+# Self destruct!!!!!!!!!!!
+# rm -- "$0"
